0a68135799857bcc36628447bde0b4b8c494a548
POB <paulblanchon9@gmail.com>
2022-09-13 19:48:25 +0200

    Update #027 v1.05 :: MediaCard
    -- Node gallery correct
    -- création du contenu card

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -10,33 +10,69 @@ export function mediasFactory(data) {
 
 
     function getUserMediasDOM(){
+        // Div principale contenant la gallery ::
         const photographGallery = document.createElement('div');
         photographGallery.setAttribute('id', 'photograph-gallery');
-
+        // Div secondaire contenant le filtre ::
         const galleryFilter = document.createElement('div');
         galleryFilter.setAttribute('id', 'gallery-filter');
-       
+        // Div secondaire contenant les médias ::
         const galleryMedias = document.createElement('div');
         galleryMedias.setAttribute('id', 'gallery-medias');
 
-        photographGallery.appendChild(galleryFilter);
-            galleryFilter.innerHTML = `
-                <label for="medias-filter">Trier par </label>
-                    <select name="medias-select" id="medias-filter">
-                        <option value="">--Please choose an option--</option>
-                        <option value="pop">Popularité</option>
-                        <option value="date">Date</option>
-                        <option value="titre">titre</option>
-                    </select>`;
         
-        photographGallery.appendChild(galleryMedias);
-        // POUR CHAQUE elmt "media", Créer un elmt <div>.
-        //  // Composé d'un elmt CONTENAIRE de l'IMG ou VIDEO.
-        //  // Composé d'un elmt LEGENDE.
-        //  //  // titre media + nbr likes.
+        photographGallery.appendChild(galleryFilter);
+        galleryFilter.innerHTML = `
+            <label for="medias-filter">Trier par </label>
+                <select name="medias-select" id="medias-filter">
+                    <option value="popu">Popularité</option>
+                    <option value="date">Date</option>
+                    <option value="titre">Titre</option>
+                </select>`;
+        
+        function mediaCard() {
+            // CARD MEDIA :: 
+            // PICTURE/VIDEO + TITLE + LIKES
+            const mediaCard = document.createElement('div');
+            mediaCard.setAttribute('class' , 'mediaCard');
+            
+                // CARD PICTURE ::
+                // Doit être un lien contenant une image
+                const linkMedia = document.createElement('a');
+                    linkMedia.setAttribute('href', `../../photographer.html?id=${id}?media=${id}`);
+                linkMedia.appendChild(mediaCard);
 
-        return photographGallery
-    }
+                const cardPict = document.createElement('div');
+                    cardPict.setAttribute('class' ,  'picture-ctn');
+
+
+                // CARD LEGEND ::  
+                // media title + number of likes
+                const cardLegend = document.createElement('div');
+                    cardLegend.setAttribute('class' , 'title-card');
+                        
+                        const mediaTitle = document.createElement('div');
+                            cardLegend.setAttribute('class' , 'media-title');
+                    
+                        const nbrLikes = document.createElement('div');
+                            cardLegend.setAttribute('class' , 'nbrLikes');
+                    
+                    mediaTitle.textContent = title;
+                    nbrLikes.textContent = likes;
+            
+                cardLegend.appendChild(mediaTitle);
+                cardLegend.appendChild(nbrLikes);
+
+            // MEDIA CARD ::
+            // Affiliation des enfants de mediaCard
+            mediaCard.appendChild(cardPict);            
+            mediaCard.appendChild(cardLegend);
+
+            return mediaCard;
+        };
+
+        return photographGallery, mediaCard;
+    };
 
     return { id, photographerId, title, image, likes, date, price, getUserMediasDOM };
 };
