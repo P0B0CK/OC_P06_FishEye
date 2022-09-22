8808ae3d6c899c4d9dc0ad2b7ed489cc22755cf9
PΩB <100569120+P0B0CK@users.noreply.github.com>
2022-09-22 22:24:44 +0200

    Update #033 v3.01 :: Photo or Video
    implantation d'un ternaire : media image ou media video.
    
    RAF :: implanter chaque photo ID

--- scripts/factories/medias.js
+++ scripts/factories/medias.js
@@ -5,92 +5,74 @@
  */
 
 export function mediasFactory(data) {
-    const { id, photographerId, title, image, likes, date, price } = data;
+    const { id, photographerId, title, image, video, likes, date, price } = data;
 
 
 
     function getUserMediasDOM(){
         // MAIN SECTION ::
-        const photographGallery = document.createElement('section');
-        photographGallery.setAttribute('id', 'photograph-gallery');
+        const mediaSection = document.querySelector(".media-section");
         
-            // // IN MAIN SECTION
-            // // TITLE
-            // const galleryFilter = document.createElement('h4');
-            // galleryFilter.setAttribute('id', 'gallery-filter');
-            // // FILTER SELECT-BOX :
-            // photographGallery.appendChild(galleryFilter);
-            // galleryFilter.innerHTML = `
-            //     <label for="medias-filter">Trier par </label>
-            //         <select name="medias-select" id="medias-filter">
-            //             <option value="popu">Popularité</option>
-            //             <option value="date">Date</option>
-            //             <option value="titre">Titre</option>
-            //         </select>`;
-            
-            // DIV GALLERY ::
-            const galleryMedias = document.createElement('div');
-            galleryMedias.setAttribute('id', 'gallery-medias');
-            photographGallery.appendChild(galleryMedias);
-            
-            // créer l'article façon innerHTML.
-            // Condition IF d'affichage selon photo/video
-            // ARTICLE MEDIACARD ::
-            const mediaCard = document.createElement('article');
-            mediaCard.setAttribute('class' , 'mediaCard');
-            mediaCard.innerHTML =`
-            <img/>
-            <h4></h4>
-            <p></p>`;
-            
-            const mediaCardLink = document.createElement('a');
-            mediaCardLink.setAttribute('href' , '')
-            
-            mediaCardLink.appendChild(mediaCard);
-            galleryMedias.appendChild(mediaCardLink);
-            
-            
-            //getPhotographerUserInfos
-            // const mediaIdPhotographer = medias.map(media => media.photographerId)
-            //const mediaOwner = media.filter(){return media.${photographerId}}
-            // for ( let i = medias.length; i >= 0; i++){
-            //     const mediaCard = document.createElement('article');
-            //     mediaCard.setAttribute('class' , 'mediaCard');
+        // ARTICLE MEDIACARD ::
+        // créer l'article façon innerHTML.
+        // Condition IF d'affichage selon photo/video
+        // mediaCard.innerHTML =`
+        //         <img src="../../assets/photographers/${photographerId}/${image}" title="${title}"/>
+        //         <h4 class="cardTitle">${title}</h4>
+        //         <p class="cardNbrLikes">${likes}</p>
+        //     `;
+            const mediaPictCard = document.createElement('article');
+            mediaPictCard.setAttribute('class' , 'media-card');
+            mediaPictCard.innerHTML =`
+                <img src="../../assets/photographers/${photographerId}/${image}" title="${title}"/>
+                <h4 class="cardTitle">${title}</h4>
+                <p class="cardNbrLikes">${likes}</p>`;
 
-            //     if(medias[i].id = i++){
-
-            //     }
-            //     return mediaCard;
-            // };
-
-
-        // MEDIA PHOTOGRAPHY ::
-        // const mediaPhoto = `<img src="../../asset/photographers/${photographerId}/${image}"/>`;
-        // MEDIA VIDEO ::
-        //const mediaVideo = `<video><source src="../../asset/photographers/${photographerId}/${video}" type="video/mp4" /></video>`;
-        // MEDIA VERIFICATION ::
-        // const mediaFormat = undefined ? mediaPhoto : mediaVideo;
-        // const mediaIdFromUser = media.map(photographerId => id);
-        // console.log(mediaIdFromUser);
-
-        const mediaPicture = document.createElement('img');
-        mediaPicture.setAttribute('src' ,  `../../asset/photographers?id=${photographerId}/${id}`);
-        mediaCard.appendChild(mediaPicture);
-
-        const mediaInfos = document.createElement('div');
-        mediaInfos.setAttribute('class' , 'mediaInfos');
-        mediaCard.appendChild(mediaInfos);
-        const mediaTitle = document.createElement('p');
-        mediaTitle.setAttribute('class' , 'card-title');
-        mediaTitle.innerHTML = `${title}`;
-        const mediaLikes = document.createElement('p');
-        mediaLikes.setAttribute('class' , 'card-likes');
-        mediaLikes.innerHTML = `${likes}`;
-        mediaInfos.appendChild(mediaTitle);
-        mediaInfos.appendChild(mediaLikes);
+            const mediaVidCard = document.createElement('article');
+            mediaVidCard.setAttribute('class' , 'media-card');
+            mediaVidCard.innerHTML =`
+            <video width="350" height="300" controls>
+                <source src="../../assets/photographers/${photographerId}/${video}" title="${title}" type=video/mp4>
+            </video>
+                <h4 class="cardTitle">${title}</h4>
+                <p class="cardNbrLikes">${likes}</p>`;
         
-        return photographGallery;
+        // TEST DE LA CONDITION ::
+        //
+        //     if (${image} == null) {
+        //         mediaVidCard.innerHTML =`
+        //             <video width="350" height="300" controls>
+        //                 <source src="../../assets/photographers/${photographerId}/${video}" title="${title}" type=video/mp4>
+        //             </video>
+        //             <video src="" title="${title}"/>
+        //             <h4 class="cardTitle">${title}</h4>
+        //             <p class="cardNbrLikes">${likes}</p>
+        //             `;
+        //     } else (${image} === ${image}) {
+        //         mediaCard.innerHTML =`
+        //             <img src="../../assets/photographers/${photographerId}/${image}" title="${title}"/>
+        //             <h4 class="cardTitle">${title}</h4>
+        //             <p class="cardNbrLikes">${likes}</p>`;
+        //     };
+        // });
+        
+        const mediaFormat = undefined ? mediaSection.appendChild(mediaPictCard) : mediaSection.appendChild(mediaVidCard);
+
+        return mediaFormat;
     };
-
+    
     return { id, photographerId, title, image, likes, date, price, getUserMediasDOM };
 };
+
+
+// MEDIA PHOTOGRAPHY ::
+// const mediaPhoto = `<img src="../../asset/photographers/${photographerId}/${image}"/>`;
+// MEDIA VIDEO ::
+//const mediaVideo = `<video><source src="../../asset/photographers/${photographerId}/${video}" type="video/mp4" /></video>`;
+// MEDIA VERIFICATION ::
+// const mediaIdFromUser = media.map(photographerId => id);
+// console.log(mediaIdFromUser);
+
+// const mediaPicture = document.createElement('img');
+// mediaPicture.setAttribute('src' ,  `../../asset/photographers?id=${photographerId}/${id}`);
+// mediaCard.appendChild(mediaPicture);
