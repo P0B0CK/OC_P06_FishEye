c401fa0eeaa9f4d901f47a39a5bc8936cbbc3033
PΩB <100569120+P0B0CK@users.noreply.github.com>
2022-11-24 10:21:35 +0100

    Update #66

--- scripts/utils/sortMedias.js
+++ scripts/utils/sortMedias.js
@@ -12,36 +12,52 @@ const selectbox = document.getElementById('selected-filter');
 
 // écouteur au changement d'état :
 selectbox.addEventListener('change', (e) => {
-    const optionValue = selectbox.value;
-    const mediasAllSection = document.querySelector('.media-section');
+  const optionValue = selectbox.value;
+  const mediasAllSection = document.querySelector('.media-section');
+  
+  // const array1 = [1, 33, 4, 21, 251];
+  // console.log(array1)
+  // array1.sort((a, b) => b - a); // ordre décroissant
+  // array1.sort((a, b) => a - b); // ordre croissant
+  // console.log(array1)
 
-
-    // const array1 = [1, 30, 4, 21, 100000];
-    // console.log(array1)
 
     if (optionValue == 'popularity') {
         console.log('=> Order by popularity <=');
-        // mediasAllSection.classList.add('hidden-media');
-        console.log(media);
-        
-        media.sort((a, b) => {
-            const x = a.likes;
-            const y = b.likes;
-            if (x < y) {
-              return -1
-            } if (x > y) {
-              return 1
-            } else { 
-                return 0 }
-          });
 
-        // array1.sort((a, b) => b - a); // ordre décroissant
-        // array1.sort((a, b) => a - b); // ordre croissant
-        // console.log(array1)
+      mediasArray.sort((a, b) => a.likes - b.likes);
+      console.log(mediasArray)
+      // FONCTION TEST ALGO
+      // array1.sort((a, b) => {
+      //   const x = a;
+      //   const y = b;
+          
+      //   return x - y; 
+      // });
+      // SIMPLIFICATION ::
+      // array1.sort((a, b) => a - b);
+      // console.log("Mon tableau croissant : " + array1);
+
     } else if (optionValue == 'date') {
-       console.log('=> Order by date <=');
+      console.log('=> Order by date <=');
+
+      mediasArray.sort((a, b) => a.date - b.date);
+      console.log(mediasArray)
+      
+      // FONCTION TEST ALGO
+      //  array1.sort((a, b) => {
+      //   const x = a;
+      //   const y = b;
+        
+      //   return y - x; 
+      // });
+      // console.log("Mon tableau décroissant : " + array1);
+
     } else if (optionValue == 'title') {
        console.log('=> Order by title <=');
+       
+       mediasArray.sort((a, b) => a.title - b.title);
+       console.log(mediasArray);
     } else {
         return console.log('== SORT issue ==');
     }
